41c4661b06e10c6a03644ae6517426fdf2d2c089
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-16 10:38:19 +0200

    Zalozeni

---
+++ IndSoftTestNuget03ThirdParty/ThirdPartyMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndSoftTestNuget03ThirdParty
+{
+  public class ThirdPartyMain
+  {
+    public string GetMyVersion()
+    { 
+      return "ThirdPartyVer=01";
+    }
+
+  }
+}
